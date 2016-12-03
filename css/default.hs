f34bd6512f7767a556b69634b1d20fc9f8cfc7c3
Paul de Rosanbo <paul@rosanbo.com>
2016-12-03 17:59:40 +0100

    styles

--- css/default.hs
+++ css/default.hs
@@ -4,14 +4,14 @@ import qualified Clay.Media as Media
 import Prelude (IO)
 
 
-bgColor, textColor, panelBgColor, textPanelColor, flashy1BgColor, flashy2BgColor :: Color
+bgColor, textColor, panelBgColor, textPanelColor, flashyColor1, flashyColor2 :: Color
 
 bgColor             = "#dfe3e9"
 textColor           = "#4a4a4a"
 panelBgColor        = "#527883"
 textPanelColor      = "#FFFFFF"
-flashy1BgColor      = "#efd117"
-flashy2BgColor      = "#f07135"
+flashyColor1        = "#efd117"
+flashyColor2        = "#f07135"
 
 
 contentSideMargin   = em 0.7
@@ -37,7 +37,7 @@ htmlbody = body ? do
         background              (setA 0.6 panelBgColor)
         nav ? do
             background          panelBgColor
-            borderBottom        solid strokeWidth flashy2BgColor
+            borderBottom        solid strokeWidth flashyColor2
             padding             (em 3) contentSideMargin (em 0.5) (em 0)
             textAlign           (alignSide sideRight)
             a ? do
@@ -49,21 +49,21 @@ htmlbody = body ? do
                 hover & color   black
 
     "#logo" ** a ? do
-        ".fa-circle" ?          color (setA 0.3 "#fff")
-        ".fa-home"   ?          color flashy1BgColor
+        ".fa-circle" ?          color (setA 0.7 "#fff")
+        ".fa-home"   ?          color "#000"
         float                   floatLeft
         fontSize                (px 14)
-        marginTop               (px 32)
+        marginTop               (px 35)
         marginLeft              contentSideMargin
-        hover & ".fa-circle" ?  color (setA 0.7 "#fff")
-        hover & ".fa-home"   ?  color "#000"
+        hover & ".fa-circle" ?  color (setA 0.3 "#fff")
+        hover & ".fa-home"   ?  color flashyColor1
 
     section # "#content" ? do
         background  (setA 0.2 textPanelColor)
         ".title" ? background   textPanelColor
         ".content" ? do
             background          (setA 0.7 textPanelColor)
-            borderBottom        solid strokeWidth flashy1BgColor
+            borderBottom        solid strokeWidth flashyColor1
 
     section # "#footer" ? do
 
@@ -93,12 +93,12 @@ htmlbody = body ? do
         textAlign               center
         img ? do
             sym borderRadius    (px 3)
-            border              solid (px 1) flashy2BgColor
+            border              solid (px 1) flashyColor2
             sym padding         (px 3)
             backgroundColor     "#fff"
         ".icons" |> a ? do
             fontSize            (px 20)
-            color               flashy2BgColor
+            color               flashyColor2
             display             inlineBlock
             marginLeft          (px 5)
             marginRight         (px 5)
@@ -107,7 +107,7 @@ htmlbody = body ? do
         ".icons" |> a # hover ? do
             color "#000"
             after & do
-                right           (pct (- 50))
+                right           (pct (- 100))
                 top             (px (- 41))
                 color           white
                 backgroundColor "#333"
@@ -118,11 +118,11 @@ htmlbody = body ? do
                 position        absolute
                 whiteSpace      nowrap
                 sym borderRadius (px 2)
-                boxShadow       0 0 (px 2) (rgba 0 0 0 0.2)
+                boxShadow       0 0 (px 4) (rgba 0 0 0 0.5)
                 zIndex          1
             before & do
-                top             (px (- 14))
-                right           (px 8)
+                top             (px (- 6))
+                right           (px 4)
                 borderStyle     solid
                 borderColor4    "#333" transparent "#333" transparent
                 borderWidth4    (px 5) (px 5) 0 (px 5)
@@ -144,12 +144,12 @@ htmlbody = body ? do
             borderColor         transparent
             transitions         [("border-color", sec 0.8, ease, sec 0.2)]
             hover & do
-                borderColor     flashy2BgColor
+                borderColor     flashyColor2
                 h3 ? color      black
                 ".info" ? color black
 
             h3 ? do
-                color           flashy2BgColor
+                color           flashyColor2
                 marginTop       (em 0.3)
                 marginBottom    (em 0.3)
 
